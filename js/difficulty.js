25276f16d12b7f7f897be121c823671aef1b0b3f
Larry Lade <lplade@users.noreply.github.com>
2016-03-07 20:25:13 -0600

    Added some licensing info

--- js/difficulty.js
+++ js/difficulty.js
@@ -1,6 +1,20 @@
-/**
- * Created by lade on 2/15/16.
- */
+/*
+Heroic-Do-Gooders calculator
+Copyright (C) 2016 l.p.lade
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 //pp. 6-7
 //Stat / Difficulty Level Divisor = % Chance of success
@@ -100,4 +114,3 @@ $(document).ready(function () {
 	//TODO responsiveness with jQuery listener is noticeably slower than JavaScript listener. Why?
 
 });
-
